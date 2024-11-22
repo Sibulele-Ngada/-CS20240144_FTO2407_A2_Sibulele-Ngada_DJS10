d0d83572a4c7581d58667d7db46fe6f487a005c4
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-22 18:42:37 +0200

    Consitionally display error

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,24 +4,35 @@ type Post = { body: string; id: number; title: string; userId: number };
 
 function App() {
   const [posts, setPosts] = useState(null);
+  const [error, setError] = useState<null | string>(null);
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts`)
-      .then((respnse) => respnse.json())
+      .then((res) => {
+        if (!res.ok) throw new Error(`Data fetching failed`);
+        return res.json();
+      })
       .then((data) => {
-        console.log(data);
+        setError(null);
         setPosts(
           data.map((post: Post) => {
-            return <li key={post.id}>{post.title}</li>;
+            return (
+              <li key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </li>
+            );
           })
         );
-      });
+      })
+      .catch((err) => setError(`Data fetching failed`));
   }, []);
 
   return (
     <>
-      <h2>Posts</h2>
+      {!error && <h2>Posts</h2>}
       <ol>{posts}</ol>
+      {error && <h1>{error}</h1>}
     </>
   );
 }
