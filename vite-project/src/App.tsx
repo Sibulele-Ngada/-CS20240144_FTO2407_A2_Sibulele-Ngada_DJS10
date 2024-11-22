8aae4ec27d821d5e4a057f8fa9cb66a254b2e2e5
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-22 13:41:58 +0200

    Successfully fetch data

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState(null);
+
+  fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then((respnse) => respnse.json())
+    .then((data) => {
+      setPosts(
+        data.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })
+      );
+    });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Posts</h2>
+      <ol>{posts}</ol>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
