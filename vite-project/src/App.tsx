b31160609972667ce84d10f41f2f5ea9282df710
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-22 13:53:17 +0200

    Wrap fetch in useEffect to prevent infinite refresh

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 
+type Post = { body: string; id: number; title: string; userId: number };
+
 function App() {
   const [posts, setPosts] = useState(null);
 
-  fetch(`https://jsonplaceholder.typicode.com/posts`)
-    .then((respnse) => respnse.json())
-    .then((data) => {
-      setPosts(
-        data.map((post) => {
-          return <li key={post.id}>{post.title}</li>;
-        })
-      );
-    });
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((respnse) => respnse.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(
+          data.map((post: Post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })
+        );
+      });
+  }, []);
 
   return (
     <>
