d319e350eee8875e20b258d24c6a43fc48c87b54
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-23 09:37:49 +0200

    Add loader

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,13 +1,18 @@
 import { useState, useEffect } from "react";
+import { PropagateLoader } from "react-spinners";
 
 type Post = { body: string; id: number; title: string; userId: number };
 
 function App() {
   const [posts, setPosts] = useState(null);
   const [error, setError] = useState<null | string>(null);
+  const [loading, setLoading] = useState(false);
 
+  //
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts`)
+    setLoading(true);
+
+    fetch(`https://jsonplaceholder.typicode.com/pots`)
       .then((res) => {
         if (!res.ok) throw new Error();
         return res.json();
@@ -27,12 +32,30 @@ function App() {
       })
       .catch(() => {
         setError(`Data fetching failed`);
+        setPosts(null);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
+
+  // Loader styles
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
 
   return (
     <>
       {!error && <h2>Posts</h2>}
+      <PropagateLoader
+        loading={loading}
+        cssOverride={override}
+        color="#4fa94d"
+        size={20}
+        aria-label="Loading Spinner"
+      />
       <ol>{posts}</ol>
       {error && <h1>{error}</h1>}
     </>
